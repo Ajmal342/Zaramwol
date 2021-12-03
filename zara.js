46830a907e1ae941078f3a8f793fea7f8dbc862a
A-J-M-A-L <88123331+Ajmal342@users.noreply.github.com>
2021-12-03 15:09:11 +0530

    Update zara.js

--- zara.js
+++ zara.js
@@ -12,7 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
- Z_NUMBER: process.env.NUMBER === undefined ? '+994400602992' : process.env.NUMBER,
- Z_OWNER: process.env.OWNER_NAME === undefined ? 'Raashii' : process.env.OWNER_NAME,
+ Z_NUMBER: process.env.NUMBER === undefined ? '+994400210394' : process.env.NUMBER,
+ Z_OWNER: process.env.OWNER_NAME === undefined ? 'Ajmal' : process.env.OWNER_NAME,
 
 };
