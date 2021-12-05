0fd019fa65d9372d80ced910795d14c30d0ce804
A-J-M-A-L <88123331+Ajmal342@users.noreply.github.com>
2021-12-05 14:59:08 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -14,10 +14,10 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('./image/r.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 
-        *Github link        _https://tinyurl.com/yggyjfgf_*
+        *Github link        _https://github.com/Ajmal342/Zaramwol_*
  
 
-       *Audio commads-1     _https://tinyurl.com/yfdkmuhd_*
+       *Audio commads-1     _https://github.com/Ajmal342/media_*
  
  
        *Audio commads-2     _https://tinyurl.com/yejp9qkh_*
